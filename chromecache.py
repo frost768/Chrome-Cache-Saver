dc5f915248ff600ec9a61900467de8b8475c030c
Emre Can Minnet <droidplus99@gmail.com>
2019-03-21 15:25:29 +0300

    Python scripts added

---
+++ chromecache.py
@@ -0,0 +1,280 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+import os, shutil, re ,binascii, gzip
+W = '\033[0m'  # Beyaz (normal)
+R = '\033[31m'  # Kırmızı
+G = '\033[32m'  # Yeşil
+O = '\033[33m'  # Turuncu
+B = '\033[34m'  # Mavi
+P = '\033[35m'  # Mor
+C = '\033[36m'  # Cyan
+GR = '\033[37m'  # Gri
+
+
+
+d = r"C:\Users\SAMSUNG\AppData\Local\Google\Chrome\User Data\Default\Cache\\"
+s = r"C:\Users\SAMSUNG\Desktop\tmp\\"
+gifs = []
+pngs = []
+jpgs = []
+mpgs = []
+gzips = []
+lav =[]
+odd = []
+byt = []
+def clean():
+ if os.path.exists(s):
+  print("\nÇalışma dizini temizleniyor...\n")
+  a=os.listdir(s)
+  for i in a:
+   if os.path.isfile(s+i) ==True:
+    os.remove(s+i)
+  a = os.listdir(s)
+  for i in a: 
+   if os.path.isfile(s+i)==False:
+    x = os.listdir(s+i)
+    for q in x:
+     os.remove(s+i+"/"+q)
+    os.rmdir(s+i)
+a=os.listdir(d)
+a.remove("data_0")
+a.remove("data_1")
+a.remove("data_2")
+a.remove("data_3")
+a.remove("index")
+
+def cop(y):
+ print("Cache kopyalanıyor...")
+ if os.path.exists(s):
+    for i in a:
+      if os.path.getsize(d+i)> int(y)*1024:
+        shutil.copy(d+i, s+i)
+ else:
+    os.mkdir(s)
+    for i in a:
+      if os.path.getsize(d+i)> int(y)*1024:
+        shutil.copy(d+i, s+i)
+
+
+
+def find(y): 
+ print("Dosya türleri bulunuyor...\n")
+ fi = os.listdir(s)
+ for i in fi:
+   if os.path.isfile(s+i) == True:
+    with open(s+i,"rb") as f:
+     z = f.read(10)
+     if re.search("JFIF", str(z)) == None and not re.search("Lav",str(z))==None:
+      lav.append(i)
+
+     if not re.search("GIF89|GIF87",str(z))==None:
+      gifs.append(i)
+
+     if not re.search("PNG", str(z)) == None:
+      pngs.append(i)
+
+     if not re.search("JFIF", str(z)) == None or not re.search("Exif", str(z)) == None:
+      jpgs.append(i)
+
+     if not re.search('FFmpeg', str(z)) == None or not re.search('ID3', str(z)) == None:
+      mpgs.append(i)
+
+     if not re.search('1f8b0', str(binascii.hexlify(z))) == None:
+       gzips.append(i)
+
+def find_gzip():
+ print("Dosya türleri bulunuyor...\n")
+ fi = os.listdir(s+"gzips")
+ for i in fi:
+   if os.path.isfile(s+"gzips\\"+i) == True:
+    with open(s+"gzips\\"+i,"rb") as f:
+     z = f.read(10)
+     if re.search("JFIF", str(z)) == None and not re.search("Lav",str(z))==None:
+      lav.append(i)
+
+     if not re.search("GIF89|GIF87",str(z))==None:
+      gifs.append(i)
+
+     if not re.search("PNG", str(z)) == None:
+      pngs.append(i)
+
+     if not re.search("JFIF", str(z)) == None or not re.search("Exif", str(z)) == None:
+      jpgs.append(i)
+
+     if not re.search('FFmpeg', str(z)) == None or not re.search('ID3', str(z)) == None:
+      mpgs.append(i)
+             
+def move():
+ if not len(gifs)==0:
+  if not os.path.exists(s+"gifs"):
+   os.mkdir(s+"gifs")
+  print("Gifler taşınıyor...\n")
+  for g in gifs:
+   shutil.move(s+g, s+"gifs/"+g+".gif")
+
+ if not len(pngs) ==0:
+  if not os.path.exists(s+"pngs"):
+   os.mkdir(s+"pngs")
+  print("Pngler  taşınıyor...\n")
+  for p in pngs:
+   shutil.move(s+p, s+"pngs/"+p+".png")
+  
+ if not len(jpgs)==0:
+  if not os.path.exists(s+"jpgs"):
+   os.mkdir(s+"jpgs")
+  print("JPEG'ler taşınıyor...\n")
+  for j in jpgs:
+   shutil.move(s+j, s+"jpgs/"+j+".jpeg")
+ 
+ if not len(lav)==0:
+  if not os.path.exists(s+"lav"):
+   os.mkdir(s+"lav")
+  for w in lav:
+   shutil.move(s+w, s+"lav/"+w+".jpeg")
+
+ if not len(mpgs)==0:
+  if not os.path.exists(s+"mpgs"):
+   os.mkdir(s+"mpgs")
+  for h in mpgs:
+   shutil.move(s+h, s+"mpgs/"+h+".mpeg")
+   
+ if not len(gzips)==0:
+  if not os.path.exists(s+"gzips"):
+   os.mkdir(s+"gzips")
+  for z in gzips:
+   shutil.move(s+z, s+"gzips/"+z+".gzip")
+   out=open(s+"gzips/"+z,"wb")
+   out.write(gzip.open(s+"gzips/"+z+".gzip","rb").read())
+   out.close()
+   os.remove(s+"gzips/"+z+".gzip")
+
+def move_gzip():
+ if not len(gifs)==0:
+  print("Gifler taşınıyor...\n")
+  for g in gifs:
+   shutil.move(s+"gzips\\"+g, s+"gifs/"+g+".gif")
+
+ if not len(pngs) ==0:
+  print("Pngler  taşınıyor...\n")
+  for p in pngs:
+   shutil.move(s+"gzips\\"+p, s+"pngs/"+p+".png")
+  
+ if not len(jpgs)==0:
+  print("JPEG'ler taşınıyor...\n")
+  for j in jpgs:
+   shutil.move(s+"gzips\\"+j, s+"jpgs/"+j+".jpeg")
+ 
+ if not len(lav)==0:
+  for w in lav:
+   shutil.move(s+"gzips\\"+w, s+"lav/"+w+".jpeg")
+
+ if not len(mpgs)==0:
+  for h in mpgs:
+   shutil.move(s+"gzips\\"+h, s+"mpgs/"+h+".mpeg")
+
+def fix():
+ print("Dosyalar düzeltiliyor...\n")
+ if not len(gifs) ==0:
+  k = os.listdir(s+"gifs/")
+  for i in k:
+   with open(s+"gifs/"+i, "r+b") as f:
+    t = str(binascii.hexlify(f.read())).split("474946")[1].split("'")
+    h = "474946"+t[0]
+    if len(h)%2!=0:
+     h =h+"0"
+     odd.append(i)
+     byt.append((t[0])[len(t[0])-5:len(t[0])])
+   with open(s+"gifs/"+i,"wb") as d:
+    d.write(binascii.unhexlify(bytes(h,"ascii")))
+ 
+ if not len(pngs)==0:
+  m = os.listdir(s+"pngs/")
+  for i in m:
+   with open(s+"pngs/"+i, "r+b") as f:
+    x = f.read()
+    y = binascii.hexlify(x)
+    z = str(y).split("504e47")
+    t = z[1].split("'")
+    h = "89504e47"+t[0]
+    if len(h)%2!=0:
+     h =h+"0"
+     byt.append(t[0][len(t[0])-5:len(t[0])])
+     odd.append(i)
+   with open(s+"pngs/"+i,"wb") as d:
+    d.write(binascii.unhexlify(bytes(h,"ascii")))
+ 
+ if not len(jpgs)==0:
+  r = os.listdir(s+"jpgs/")
+  for i in r:
+   with open(s+"jpgs/"+i, "r+b") as f:
+    x = f.read()
+    y = binascii.hexlify(x)
+    z = str(y).split("4a464946")
+    if len(z)==3:
+     t = z[2].split("'")
+     h = "ffd8ffe0001045786966"+z[1]+"ffd8ffe0001045786966"+t[0]
+    else:
+     t = z[1].split("'")
+     h = "ffd8ffe0001045786966"+t[0]
+    if len(h)%2!=0:
+     h =h+"0"
+     odd.append(i)
+     byt.append((t[0])[len(t[0])-5:len(t[0])])
+    with open(s+"jpgs/"+i,"wb") as d:
+     d.write(binascii.unhexlify(bytes(h,"ascii")))
+ 
+ if not len(lav)==0:
+  y = os.listdir(s+"lav/")
+  for i in y:
+   with open(s+"lav/"+i, "r+b") as f:
+    x = f.read()
+    y = binascii.hexlify(x)
+    z = str(y).split("4c617663")
+    t = z[1].split("'")
+    h = "ffd8ffe000104c617663"+t[0]
+    if len(h)%2!=0:
+     h =h+"0"
+     odd.append(i)
+     byt.append((t[0])[len(t[0])-5:len(t[0])])
+   with open(s+"lav/"+i,"wb") as d:
+    d.write(binascii.unhexlify(bytes(h,"ascii")))
+   if not os.path.exists(s+"jpgs"):
+    os.mkdir(s+"jpgs")
+    
+   shutil.move(s+"lav/"+i, s+"jpgs/"+i)
+  os.rmdir(s+"lav")
+
+def clean2():
+ print("Temizleniyor...\n")
+ j = os.listdir(s)
+ for i in j:
+  if os.path.isfile(s+i)==True:
+   os.remove(s+i)
+ j = os.listdir(s+"gzips\\")
+ for i in j:
+  if os.path.isfile(s+"gzips\\"+i)==True:
+   os.remove(s+"gzips\\"+i)
+ print("Bütün dosyalar ayrıştırıldı!")
+
+y= input("Gireceğiniz KB değerinden küçük olan dosyalar silinecek:")
+clean()
+cop(y)
+find(y)
+move()
+lav=[]
+gifs=[]
+pngs=[]
+jpgs=[]
+mpgs=[]
+find_gzip()
+move_gzip()
+#fix()
+clean2()
+print(pngs)
+print(gifs)
+print(jpgs)
+if not len(odd)==0:
+ for i in odd:
+  for n in byt:
+   print("Düzenlenen dosya:"+i+" Son 5 byte:"+n)
---
+++ mediacache.py
@@ -0,0 +1,187 @@
+﻿#An application for extracting video files from media cache of Google Chrome
+#ChrNolan52
+
+import os, re, binascii, struct
+#Defining variables and constants
+d = r"C:\Users\SAMSUNG\AppData\Local\Google\Chrome\User Data\Profile 13\Cache\\"
+#d = r"C:\Users\SAMSUNG\AppData\Local\Google\Chrome\User Data\Default\Cache\\"
+s = r"C:\Users\SAMSUNG\Desktop\tmp2\\"
+files = []
+bins = []
+addr = []
+parts = []
+urldict={}
+hexdict={}
+clean=[]
+def get_files():
+  l=os.listdir(d)
+  l.remove("data_0")
+  l.remove("data_1")
+  l.remove("data_2")
+  l.remove("data_3")
+  l.remove("index")
+
+  for i in l:
+    files.append(i.split("f_")[1])
+
+#Dosya isimlerini binary türüne çevirme
+def conv2bin():
+  for i in files:
+    b=""
+    for ix in range(0,6):
+      b+=bin(int(i[ix],16))[2:].zfill(4)
+    bins.append([i,"10000000"+b])
+
+#Binary türü isimleri little endian a cevirme
+def bin2le():
+  for b in bins:
+    little_endian=struct.pack("<L",int("80"+b[0],16))
+    little_endian_b=str(little_endian).replace("\\x","").split("'")[1]
+    #print("hex_string:"+ str(hex_string).replace("\\x","")+" little:"+str(little_endian).replace("\\x","").split("'")[1]+" len:"+str(len(str(little_endian).replace("\\x","").split("'")[1])))
+
+    if len(little_endian_b)==8 and not ((little_endian_b[0]).encode("utf-8")).hex()=="5c":
+      addr.append([b[0],little_endian_b])
+
+    if len(little_endian_b)==7:
+      if (little_endian_b)[6]=='"':
+        addr.append([b[0],"27010080"])
+      else:
+        first_char=((little_endian_b[0]).encode("utf-8")).hex()
+        addr.append([b[0],first_char+(little_endian_b)[1:]])
+        
+    if len(little_endian_b)==8 and ((little_endian_b[0]).encode("utf-8")).hex()=="5c":
+      if little_endian_b[1]=='t':
+         addr.append([b[0],"09"+(little_endian_b)[2:]])
+
+      if little_endian_b[1]=='n':
+         addr.append([b[0],"0a"+(little_endian_b)[2:]])
+         
+      if little_endian_b[1]=='r':
+         addr.append([b[0],"0d"+(little_endian_b)[2:]])
+
+      if little_endian_b[1]=='\\':
+         addr.append([b[0],"5c"+(little_endian_b)[2:]])
+       
+#Data dosyasını okuma ve hex türüne cevirme
+def read_data():
+  with open(d+"\\data_1",mode="rb") as f:
+    hexed = binascii.hexlify(f.read())
+  return hexed
+
+#Olusturulan adreslere gore url ve sıra alma
+def sort_urls(hexed):
+  for adres in addr:
+      try:
+        beginning=re.search(str(adres[1]),str(hexed)).start()
+        http_flag=re.search("68747470",str(hexed[beginning:])).start()
+        beginning=beginning+http_flag-2
+        end=re.search("3a......0",str(hexed[beginning:])).start()
+        hexed_url=hexed[beginning:beginning+end+6]
+##    FOR DEBUG PURPOSES
+
+##        print("adres: "+adres[1]+" beg: "+str(beginning)+" end: "+str(end)+" beg+end: "+str(beginning+end)+ \
+##         "\n------------------------------------------------\n"+ \
+##              str(binascii.unhexlify(hexed_url)).replace("\\x00","").split("'")[1] + \
+##         "\n-----------------------------------------------")
+        
+        byte_data=str(hexed_url).split("'")[1][-6:]
+        byte1=str(binascii.unhexlify(byte_data[0:2])).split("'")[1]
+        byte2=str(binascii.unhexlify(byte_data[2:4])).split("'")[1]
+        byte3=str(binascii.unhexlify(byte_data[4:6])).split("'")[1]
+##        print("byte_data: "+byte_data+" ve "+byte1+" ve "+byte2+" ve "+byte3+"\n")
+      except:
+        print("hata "+ adres[1]+ byte_data[0]+" "+ str(beginning)+" "+ str(end)+ " bulunamadı")
+
+      url_base=str(binascii.unhexlify(hexed_url)).replace("\\x00","").split("'")[1]
+      if re.search("webm",url_base) is None:
+        if byte2=="\\x00" and byte3=="\\x00":
+          url = url_base[:-2]
+          p=["f_"+adres[0],url,byte1]
+        if byte3 == "\\x00" and byte2!="\\x00":
+          url = url_base[:-3]
+          p=["f_"+adres[0],url,byte1+byte2]
+        if byte3 != "\\x00" and byte2!="\\x00":
+          url = url_base[:-4]
+          p=["f_"+adres[0],url,byte1+byte2+byte3]
+        parts.append(p)
+
+
+#URL gruplandirma
+def categorize():
+  for i in parts:
+    for n in parts:
+      if not n[1] == i[1] and not n[1] in clean:
+        clean.append(n[1])
+      else:
+        clean.append(n[1])
+
+  #gruplardan liste olusturma
+  for i in clean:
+    urldict[i] = []
+    
+  for url in urldict:
+  #   gruplandirilmis url listelerine dosya parcalarinin indexlerini ekleme
+      for member in range(0,len(parts)):
+        if url==parts[member][1]:
+          urldict[url].append(member)
+      hexdict[url]=[]
+      
+  #   hexadecimal olarak belirtilen indexleri inte cevirme
+      for partFileIndex in urldict[url]:
+        if not len(urldict[url])==1:
+          hexdict[url].append([partFileIndex,int(str(parts[partFileIndex][2]),16)])
+  #   int haline gelen indexleri siralama
+      hexdict[url]=sorted(hexdict[url],key = lambda x:int(x[1]))
+  ##    FOR DEBUG PURPOSES
+      if re.search("mp4",url):
+        name=url.split(".mp4")[0][[(m.end()) for m in re.finditer("/",url.split(".mp4")[0])][-1]:].replace("\\","").replace("/","")
+      elif re.search("mp3",url):
+        name=url.split(".mp3")[0][[(m.end()) for m in re.finditer("/",url.split(".mp3")[0])][-1]:].replace("\\","").replace("/","")
+      else:   
+         name=url.split(".php")[0][[(m.end()) for m in re.finditer("/",url.split(".php")[0])][-1]:].replace("\\","").replace("/","")
+      print("Video: "+name+"\nURL: "+url+"\n-----------------------------\nParts: ")
+      b=1
+      for i in hexdict[url]:
+        if (b%7)==0:
+          print(parts[i[0]][0])
+        else:
+          print(parts[i[0]][0],end=" ")
+        b+=1
+      print("\n")
+        
+##  Klasor olusturma
+      if not os.path.exists(s):
+        os.mkdir(s)
+  ##  Dosyaları sırasıyla birleştirme
+      if not os.path.isfile(s+name+".mp4"):
+        with open(s+name+".mp4",mode="wb") as mp4:
+          for i in hexdict[url]:
+            with open(d+parts[i[0]][0],mode="rb") as file:
+              mp4.write(file.read())
+              
+      elif not os.path.isfile(s+name+"(2).mp4"):
+        with open(s+name+"(2).mp4",mode="wb") as mp4:
+          for i in hexdict[url]:
+            with open(d+parts[i[0]][0],mode="rb") as file:
+              mp4.write(file.read())
+
+      else:
+        with open(s+name+"(3).mp4",mode="wb") as mp4:
+          for i in hexdict[url]:
+            with open(d+parts[i[0]][0],mode="rb") as file:
+              mp4.write(file.read())
+def clean_dir():
+  a=os.listdir(s)
+  for i in a:
+    if os.path.getsize(s+i)==0:
+      os.remove(s+i)
+
+def calistir():
+  get_files()
+  conv2bin()
+  bin2le()
+  sort_urls(read_data())
+  categorize()
+  #clean_dir()
+calistir()
+input()
